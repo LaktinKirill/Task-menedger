05a5d4c108e3c38157a0afdf3a941302e0d23ed2
LaktinKirill <164017355+LaktinKirill@users.noreply.github.com>
2024-04-01 17:28:21 +0300

    Задание зоопарк

--- inheritance_class.py
+++ inheritance_class.py
@@ -1,38 +1,72 @@
-class User():
-    def __init__(self, id, user_name, level):
-        self._id = id
-        self._user_name = user_name
-        self._level = ("user")
+class Animal():
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+    def make_sound(self):
+        pass
+    def eat(self):
+        pass
+class Bird(Animal):
+    def __init__(self, food, name, age):
+        super().__init__(name, age)
+        self.food = food
+    def eat(self):
+        print(f"{self.name}, ест {self.food}")
+    def make_sound(self):
+        print("Кря-кря")
+class Mammal(Animal):
+    def __init__(self, food, name, age):
+        super().__init__(name, age)
+        self.food = food
+    def eat(self):
+        print(f"{self.name}, ест {self.food}")
+    def make_sound(self):
+        print("Рррррррррр")
+class Reptile(Animal):
+    def __init__(self, food, name, age):
+        super().__init__(name, age)
+        self.food = food
+    def eat(self):
+        print(f"{self.name}, ест {self.food}")
+    def make_sound(self):
+        print("Гррррррррр")
 
-    def get_id(self):
-        return self._id
+def animal_sound(animals):
+    for animal in animals:
+        animal.make_sound()
+def eat_food(animals):
+    for animal in animals:
+        animal.eat()
+animals = [Bird( "Семечки", "Кукушка", 2), Mammal("Мясо", "Собака", 4), Reptile("Рыба", "Крокодил", 3)]
 
-    def get_name(self):
-        return self._user_name
-
-    def get_access_level(self):
-        return self._level
-
-    def set_name(self, user_name):
-        self._user_name = user_name
-class Admin(User):
-    def __init__(self, id, user_name):
-        super().__init__(id, user_name)
-        self._level = 'admin'
-        self._users_list = []
-    def add_user(self):
-        if isinstance(id, User) and id not in self._users_list:
-            self._users_list.append(id)
-            print(f"Пользователь '{id.get_name()}' успешно добавлен.")
-        else:
-            print("Не удалось добавить пользователя.")
-    def remove_user(self, id):
-        if id in self._users_list:
-            self._users_list.remove(id)
-            print(f"Пользователь '{id.get_name()}' успешно удален.")
-        else:
-            print("Пользователь не найден.")
+animal_sound(animals)
+eat_food(animals)
 
 
+class Zoo:
+    def __init__(self):
+        self.animals = []
+        self.staff = []
 
+    def add_animal(self, animal):
+        self.animals.append(animal)
+
+    def add_staff(self, staff_member):
+        self.staff.append(staff_member)
+
+
+class ZooKeeper:
+    def __init__(self, name):
+        self.name = name
+
+    def feed_animal(self, animal):
+        print(f"{self.name} is feeding {animal.name}.")
+
+
+class Veterinarian:
+    def __init__(self, name):
+        self.name = name
+
+    def heal_animal(self, animal):
+        print(f"{self.name} is healing {animal.name}.")
 
